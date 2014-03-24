3d9672ab43c6c0d149d69cbf9dba393f94e2768c
wise_joker <jayzjj000@163.com>
2014-03-24 11:33:31 +0800

    init commit

--- cameraForBingyanV1-0/ViewController.h
+++ cameraForBingyanV1-0/ViewController.h
@@ -7,7 +7,46 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "GPUImage.h"
+#import "AppDelegate.h"
 
-@interface ViewController : UIViewController
+#define FRAME_RECODEBUTTON CGRectMake(105, 404, 100, 40)
+#define FRAME_IMAGEVIEW CGRectMake(0, 0, 320, 460)
+#define FRAME_LABEL CGRectMake(0, 0, 200, 50)
+#define FRAME_TABLEVIEW CGRectMake(108, 335, 105, 320)
+
+#define STRING_STARTRECORDING @"开始录制"
+#define STRING_STOPRECORDING @"停止录制"
+#define STRING_SAVERECORDING @"保存录像"
+
+#define STRING_PATHTOMOVIE @"Documents/Movie1.m4v"
+
+#define LABEL_RECORDING @"正在录制..."
+
+@interface ViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>{
+    GPUImageMovieWriter * movieWriter;
+    GPUImageVideoCamera * videoCamera;
+    GPUImageOutput<GPUImageInput> * testFilter;
+    GPUImageView * imageView;
+    
+    NSMutableDictionary * videoSettings;
+    NSDictionary * audioSettings;
+
+    
+    NSString * pathToMovie;
+    
+    UIButton * recodeButton;
+    UILabel * stateLabel;
+    
+    int timesHaveRecorded;
+}
+
+-(IBAction)onPressRecodeButton:(id)sender;
+
+-(void)initializeRecoderWith:(NSString *)thePath;
+
+//tableView
+@property (nonatomic, retain) NSArray *dataList;
+@property (nonatomic, retain) UITableView *myTableView;
 
 @end
--- cameraForBingyanV1-0/ViewController.m
+++ cameraForBingyanV1-0/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
 
 @end
@@ -17,7 +18,123 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    //init times
+    timesHaveRecorded = 0;
+    
+    //init Label
+    stateLabel = [[UILabel alloc] initWithFrame:FRAME_LABEL];
+    stateLabel.text = LABEL_RECORDING;
+    stateLabel.textColor = [UIColor redColor];
+    stateLabel.backgroundColor = [UIColor clearColor];
+    
+    //init ImageView
+    imageView = [[GPUImageView alloc]init];
+    imageView.frame = FRAME_IMAGEVIEW;
+    [self.view addSubview:imageView];
+    
+    //init Button
+    recodeButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    recodeButton.frame = FRAME_RECODEBUTTON;
+    [recodeButton setTitle:STRING_STARTRECORDING forState:UIControlStateNormal];
+    [recodeButton addTarget:self action:@selector(onPressRecodeButton:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:recodeButton];
+    
+    
+    
+    
+    //init Video Setting
+    videoSettings = [[NSMutableDictionary alloc] init];;
+    [videoSettings setObject:AVVideoCodecH264 forKey:AVVideoCodecKey];
+    [videoSettings setObject:[NSNumber numberWithInteger:200] forKey:AVVideoWidthKey];
+    [videoSettings setObject:[NSNumber numberWithInteger:200] forKey:AVVideoHeightKey];
+    
+    //init audio setting
+    AudioChannelLayout channelLayout;
+    memset(&channelLayout, 0, sizeof(AudioChannelLayout));
+    channelLayout.mChannelLayoutTag = kAudioChannelLayoutTag_Stereo;
+    
+    audioSettings = [NSDictionary dictionaryWithObjectsAndKeys:
+                                   [ NSNumber numberWithInt: kAudioFormatMPEG4AAC], AVFormatIDKey,
+                                   [ NSNumber numberWithInt: 2 ], AVNumberOfChannelsKey,
+                                   [ NSNumber numberWithFloat: 16000.0 ], AVSampleRateKey,
+                                   [ NSData dataWithBytes:&channelLayout length: sizeof( AudioChannelLayout ) ], AVChannelLayoutKey,
+                                   [ NSNumber numberWithInt: 32000 ], AVEncoderBitRateKey,
+                                   nil];
+    //init VideoCamera
+    videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionBack];
+    videoCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+
+    [videoCamera startCameraCapture];
+
+    //init url
+    [self initializeRecoderWith:STRING_PATHTOMOVIE];
+//    //init Movie path
+//    pathToMovie = [NSHomeDirectory() stringByAppendingPathComponent:STRING_PATHTOMOVIE];
+//    unlink([pathToMovie UTF8String]); // If a file already exists, AVAssetWriter won't let you record new frames, so delete the old movie
+//    NSURL *movieURL = [NSURL fileURLWithPath:pathToMovie];
+//    
+//    //init movieWriter
+//    movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:movieURL size:CGSizeMake(480.0, 640.0) fileType:AVFileTypeMPEG4 outputSettings:videoSettings];
+//    
+//    [movieWriter setHasAudioTrack:YES audioSettings:audioSettings];
+//    
+//    
+//    //init VideoCamera
+//    videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionBack];
+//    videoCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+//    
+//    //init Filter
+//    testFilter = [[GPUImageSketchFilter alloc] init];
+//    
+//    [videoCamera addTarget:testFilter];
+//    [testFilter addTarget:imageView];
+//    [testFilter addTarget:movieWriter];
+//    [videoCamera startCameraCapture];
+//    
+    
+    
+    
+    //init tableView
+    NSArray *list = [NSArray arrayWithObjects:@"模糊",@"素描",@"怀旧", nil];
+    self.dataList = list;
+    
+    UITableView *tableView = [[UITableView alloc] initWithFrame:FRAME_TABLEVIEW style:UITableViewStylePlain];
+    // 设置tableView的数据源
+    tableView.dataSource = self;
+    // 设置tableView的委托
+    tableView.delegate = self;
+     tableView.transform = CGAffineTransformMakeRotation(-M_PI / 2);
+    tableView.showsVerticalScrollIndicator = NO;
+    // 设置tableView的背景图
+//    tableView.backgroundView = [[UIImageView alloc] init];
+    self.myTableView = tableView;
+    self.myTableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    [self.view addSubview:self.myTableView];
+    
+}
+
+- (void)initializeRecoderWith:(NSString *)thePath
+{
+    //init Movie path
+    pathToMovie = [NSHomeDirectory() stringByAppendingPathComponent:thePath];
+    unlink([pathToMovie UTF8String]); // If a file already exists, AVAssetWriter won't let you record new frames, so delete the old movie
+    NSURL *movieURL = [NSURL fileURLWithPath:pathToMovie];
+    
+    //init movieWriter
+    movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:movieURL size:CGSizeMake(480.0, 640.0) fileType:AVFileTypeMPEG4 outputSettings:videoSettings];
+    
+    [movieWriter setHasAudioTrack:YES audioSettings:audioSettings];
+    
+    
+
+    
+    //init Filter
+    testFilter = [[GPUImageSketchFilter alloc] init];
+    
+    [videoCamera addTarget:testFilter];
+    [testFilter addTarget:imageView];
+    [testFilter addTarget:movieWriter];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +143,204 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - vidoe camera
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+{
+    // Map UIDeviceOrientation to UIInterfaceOrientation.
+    UIInterfaceOrientation orient = UIInterfaceOrientationPortrait;
+    switch ([[UIDevice currentDevice] orientation])
+    {
+        case UIDeviceOrientationLandscapeLeft:
+            orient = UIInterfaceOrientationLandscapeLeft;
+            break;
+            
+        case UIDeviceOrientationLandscapeRight:
+            orient = UIInterfaceOrientationLandscapeRight;
+            break;
+            
+        case UIDeviceOrientationPortrait:
+            orient = UIInterfaceOrientationPortrait;
+            break;
+            
+        case UIDeviceOrientationPortraitUpsideDown:
+            orient = UIInterfaceOrientationPortraitUpsideDown;
+            break;
+            
+        case UIDeviceOrientationFaceUp:
+        case UIDeviceOrientationFaceDown:
+        case UIDeviceOrientationUnknown:
+            // When in doubt, stay the same.
+            orient = fromInterfaceOrientation;
+            break;
+    }
+    videoCamera.outputImageOrientation = orient;
+    
+}
+
+
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES; // Support all orientations.
+}
+
+
+#pragma mark - responder
+
+- (IBAction)onPressRecodeButton:(id)sender
+{
+
+    
+    NSString * title = [recodeButton titleForState:normal];
+    
+    if ([title isEqualToString:STRING_STARTRECORDING]) {
+        NSString * tempPath = [NSString stringWithFormat:@"Documents/Movie%d.m4v",timesHaveRecorded];
+
+        //reset url and movieWriter
+        [testFilter removeAllTargets];
+        [videoCamera removeAllTargets];
+        [self initializeRecoderWith:tempPath];
+        
+        // ui sets
+        [recodeButton setTitle:STRING_SAVERECORDING forState:UIControlStateNormal];
+        [self.view addSubview:stateLabel];
+        [stateLabel setAlpha:1.0];
+
+        //start recording
+        NSLog(@"start recording");
+        videoCamera.audioEncodingTarget = movieWriter;
+        [movieWriter startRecording];
+
+        //error
+        NSError * error = nil;
+        if (![videoCamera.inputCamera lockForConfiguration:&error])
+        {
+            NSLog(@"Error locking for configuration: %@", error);
+        };
+        
+        //flashLight
+//        [videoCamera.inputCamera setTorchMode:AVCaptureTorchModeOn];
+//        [videoCamera.inputCamera unlockForConfiguration];
+        
+    }
+    else if ([title isEqualToString:STRING_SAVERECORDING]){
+        //ui sets
+        [recodeButton setTitle:STRING_STARTRECORDING forState:UIControlStateNormal];
+        [stateLabel removeFromSuperview];
+
+        //stop recording
+        [testFilter removeTarget:movieWriter];
+        videoCamera.audioEncodingTarget = nil;
+        [movieWriter finishRecording];
+        NSLog(@"Movie completed");
+        
+        //error
+        [videoCamera.inputCamera lockForConfiguration:nil];
+//        [videoCamera.inputCamera setTorchMode:AVCaptureTorchModeOff];
+//        [videoCamera.inputCamera unlockForConfiguration];
+
+        UISaveVideoAtPathToSavedPhotosAlbum(pathToMovie, nil, nil, nil);
+        UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"提示~" message:@"保存成功" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+        [alert show];
+
+    }
+//    else{
+//        UISaveVideoAtPathToSavedPhotosAlbum(pathToMovie, nil, nil, nil);
+//        UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"提示~" message:@"保存成功" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+//        [alert show];
+//    }
+}
+
+
+
+
+#pragma mark - Table View
+
+//set cells
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellWithIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellWithIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellWithIdentifier];
+    }
+    cell.imageView.image = [UIImage imageNamed:@"example.png"];
+    return cell;
+}
+
+//set number of sections
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+//行首缩进
+- (NSInteger)tableView:(UITableView *)tableView indentationLevelForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 0;
+}
+
+//row height
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 105;
+}
+
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+//    if ([indexPath row] % 2 == 0) {
+//        cell.backgroundColor = [UIColor blueColor];
+//    } else {
+//        cell.backgroundColor = [UIColor greenColor];
+//    }
+}
+
+
+//select rows callback
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [videoCamera removeAllTargets];
+    [testFilter removeAllTargets];
+
+    
+    switch ([indexPath row]) {
+        case 0:
+            testFilter = [[GPUImageFastBlurFilter alloc] init];
+            break;
+        case 1:
+            testFilter = [[GPUImageSketchFilter alloc] init];
+            break;
+        case 2:
+            testFilter = [[GPUImageSepiaFilter alloc] init];
+            break;
+        default:
+            break;
+    }
+    
+
+    [videoCamera addTarget:testFilter];
+    [testFilter addTarget:imageView];
+    [testFilter addTarget:movieWriter];
+
+}
+
+
+// delete
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"执行删除操作");
+}
+
+//设置没有分界线
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return UITableViewCellEditingStyleNone;
+}
+
+
+//number of rows
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.dataList.count;
+}
 @end
